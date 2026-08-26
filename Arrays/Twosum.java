422f41d10b0c189d248ce9e4f390ba456405fc02
sangusajjan <sangmesh.sajjanshetty1@gmail.com>
2026-08-26 13:22:11 +0530

    Add Two Sum solution

---
+++ Arrays/Twosum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int needed = target - nums[i];
+
+            if (map.containsKey(needed)) {
+                return new int[]{map.get(needed), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
